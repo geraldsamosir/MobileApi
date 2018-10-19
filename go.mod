03d364bb69679d7154c4895876bf3e2a2a7d98a2
Gerald Halomoan Samosir <geraldsamosir@ShadowWarrior.local>
2018-10-19 15:01:01 +0700

    add config

--- go.mod
+++ go.mod
@@ -1,1 +1,7 @@
 module github.com/geraldsamosir/MobileApi.git
+
+require (
+	github.com/gorilla/mux v1.6.2
+	github.com/jinzhu/gorm v1.9.1 // indirect
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func main(){
+type Message struct {
+	Mymessage string
+}
+
+func (m *Message) Test(res http.ResponseWriter, req *http.Request) {
+	m.Mymessage = "message ok from modules"
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(200)
+	json.NewEncoder(res).Encode(m)
+}
+
+func main() {
 	fmt.Println("hello this is from  package golang using go mod ")
+	var m Message
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", m.Test)
+
+	http.ListenAndServe("0.0.0.0:5000", r)
 }
