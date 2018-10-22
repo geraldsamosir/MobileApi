1a7b709d0e4845928cad2d94c71f57fa74dc7624
Gerald Halomoan Samosir <geraldsamosir@ShadowWarrior.local>
2018-10-22 19:23:55 +0700

    update lagi

--- Controller/article.go
+++ Controller/article.go
@@ -14,7 +14,8 @@ type Article struct {
 func (a *Article) Get(res http.ResponseWriter, req *http.Request) {
 	a.Title = "hello"
 	a.Body = "this is body"
-	a.Tags = append(a.Tags, "ok")
+	var tags []string
+	a.Tags = append(tags, "ok")
 	res.Header().Set("Content-Type", "application/json")
 	res.WriteHeader(200)
 	json.NewEncoder(res).Encode(a)
