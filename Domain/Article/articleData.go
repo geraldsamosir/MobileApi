a51a9f337aef60d532b09066efaae9918fa460cd
Gerald Halomoan Samosir <geraldsamosir@ShadowWarrior.local>
2018-10-23 18:36:39 +0700

    add model

--- Domain/Article/articleData.go
+++ Domain/Article/articleData.go
@@ -1,1 +1,19 @@
 package Article
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type ArticleData struct {
+	gorm.Model
+	Title       string    `gorm:"column:title;not null"`
+	Body        string    `gorm:"column:body;not null"`
+	Banner      string    `gorm:"column:banner;not null"`
+	DatePublish time.Time `gorm:"column:datePublish;not null;DEFAULT:current_timestamp"`
+	Is_draf     bool      `gorm:"column:is_draf;not null;default:true"`
+	Is_publish  bool      `gorm:"column:is_publish;not null;default:false"`
+	// Topics      []Topics  `gorm:"many2many:news_topics;save_association:false"`
+}
--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module github.com/geraldsamosir/MobileApi
 
 require (
+	github.com/go-sql-driver/mysql v1.4.0 // indirect
 	github.com/gorilla/mux v1.6.2
-	github.com/jinzhu/gorm v1.9.1 // indirect
+	github.com/jinzhu/gorm v1.9.1
 	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
 	github.com/spf13/viper v1.2.1
 )
